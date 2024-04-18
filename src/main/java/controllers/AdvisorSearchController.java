196cc76a0a1ac530fcbb5b52e097f3d2b1e135c8
Aarsh Patel <aarshpatel2004@gmail.com>
2024-04-18 17:43:00 -0400

    Fix Search

--- src/main/java/controllers/AdvisorSearchController.java
+++ src/main/java/controllers/AdvisorSearchController.java
@@ -8,10 +8,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
@@ -58,12 +60,23 @@ public class AdvisorSearchController {
         ArrayList<Student> searchedStudents = degreeWork.advisorSearchStudents(searchText);
         studentList.getChildren().clear();
         studentList.setSpacing(10);
-        for(Student student : searchedStudents) {
-            // Create a new TextField for each student
-            TextField studentTextField = new TextField(student.getFirstName());
-            studentTextField.setEditable(false);
-            studentList.getChildren().add(studentTextField);
+        for (Student student : searchedStudents) {
+            HBox hbox = new HBox(10); // 10 is the spacing between elements in the HBox
+            hbox.setStyle("-fx-background-color: white;");  // Set the background color to white
+        
+            Label studentLabel = new Label(student.getFirstName() + " " + student.getLastName());
+            Button viewButton = new Button("View Details");
+            viewButton.setOnAction(e -> showStudentDetails(student));
+        
+            hbox.getChildren().addAll(studentLabel, viewButton);
+            studentList.getChildren().add(hbox);
         }
-        studentList.getChildren().add(studentList);
+        
+    }
+    void showStudentDetails(Student student) {
+        // Implement navigation or display logic here
+        // For example, change a pane in your UI to show details about the student
+        System.out.println("Showing details for: " + student.getFirstName() + " " + student.getLastName());
+        // This could be navigating to another view or updating a detail section on the current page
     }
 }
