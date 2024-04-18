fb01ad455288d962d4d3dfc30823176f29c81065
pakshpatel1@gmail.com <pakshpatel1@gmail.com>
2024-04-18 17:00:25 -0400

    clean

--- src/main/java/model/ClassLevel.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public enum ClassLevel {
-    FRESHMAN,
-    SOPHOMORE,
-    JUNIOR,
-    SENIOR
-}
--- src/main/java/model/Season.java
+++
@@ -1,6 +0,0 @@
-package model;
-public enum Season {
-    SPRING,
-    SUMMER,
-    FALL
-}
