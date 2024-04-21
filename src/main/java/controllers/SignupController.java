08cb3e4c2c7bd0d3a6897c3c687802e0fc3d748c
pakshpatel1@gmail.com <pakshpatel1@gmail.com>
2024-04-21 16:20:06 -0400

    signup working

--- src/main/java/controllers/SignupController.java
+++ src/main/java/controllers/SignupController.java
@@ -1,85 +1,136 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.ResourceBundle;
 
 import aisle.App;
-
-import java.net.URL;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import model.DegreeWork;
 
-public class SignupController implements Initializable {
-    @FXML
-    private TextField txt_firstName;
+public class SignupController {
 
     @FXML
-    private TextField txt_lastName;
+    private HBox Hbox;
 
     @FXML
-    private TextField txt_email;
+    private HBox HboxLeft;
 
     @FXML
-    private TextField txt_password;
+    private Button backButton;
 
     @FXML
-    private TextField txt_confirmPassword;
+    private ImageView background;
 
-    // @FXML
-    // private TextField txt_studentID;
+    @FXML
+    private PasswordField confirmPassword;
 
-    // @FXML
-    // private Label signup_error;
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private Text goToLogIn;
+
+    @FXML
+    private CheckBox isAdvisorCheckbox;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private Label login_error;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private AnchorPane parent;
+
+    @FXML
+    private Button signedUp;
+
+    @FXML
+    private StackPane stackPane;
+
+    @FXML
+    private StackPane stackPaneLeft;
+
+    @FXML
+    private TextField uscID;
+
+    @FXML
+    private TextField userEmail;
+
+    @FXML
+    private PasswordField userPassword;
+
+    @FXML
+    private VBox vboxLoginInfo;
+
+    @FXML
+    private Text welcome;
+
+    @FXML
+    private Text welcome1;
+
+    @FXML
+    private Label signup_error;
+
+    @FXML
+    void goBack(ActionEvent event) throws IOException{
+        App.setRoot("landing_page");
+    }
+
+    @FXML
+    void goToLoginPage(MouseEvent event) throws IOException{
+        App.setRoot("login_page");
+    }
 
     @FXML
     void signupClicked(ActionEvent event) throws IOException {
-        String firstName = txt_firstName.getText();
-        String lastName = txt_lastName.getText();
-        String email = txt_email.getText();
-        String password = txt_password.getText();
-        String confirmPassword = txt_confirmPassword.getText();
-        //String studentID = txt_studentID.getText();
+        String userfirstName = firstName.getText();
+        String userlastName = lastName.getText();
+        String email = userEmail.getText();
+        String password = userPassword.getText();
+        String userconfirmPassword = confirmPassword.getText();
+        String studentID = uscID.getText();
+        boolean isAdvisor = isAdvisorCheckbox.isSelected();
 
-        if(firstName.equals("") || lastName.equals("") || email.equals("")
-            || password.equals("") || confirmPassword.equals("")) 
-        {
-            // signup_error.setText("Not blank, Bruh!");
-            System.out.println("Not blank, Bruh!");
+        if(userfirstName.equals("") || userlastName.equals("") || email.equals("")
+            || password.equals("") || studentID.equals("")) {
+            signup_error.setText("Not blank, Bruh!");
             return;
         }
 
-        if(!password.equals(confirmPassword)) {
-            // signup_error.setText("Confirm password must be same, Bruh!");
-            System.out.println("Confirm password must be same, Bruh!");
+        if(!password.equals(userconfirmPassword)) {
+            signup_error.setText("Confirm password must be same, Bruh!");
             return;
         }
 
         DegreeWork degreeWork = DegreeWork.getInstance();
 
-        if(!degreeWork.signup(firstName, lastName, email, password, "X83012475", false)) {
-            // signup_error.setText("Account already exists.");
-            System.out.println("Account already exists.");
+        if(!degreeWork.signup(userfirstName, userlastName, email, password, studentID, isAdvisor)) {
+            signup_error.setText("Account already exists.");
             return;
         }
 
+        if(studentID.chars().toArray().length != 9) {
+            signup_error.setText("Student ID must be 9 digits.");
+            return;
+        }
+
+        signup_error.setText(" ");
         App.setRoot("student_dashboard_page");
     }
-
-    @FXML
-    private void back(ActionEvent event) throws IOException {
-        App.setRoot("landing_page");
-    }
-    
-    private void sizes () {
-        
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-
-    }
-}
+}   
